71c52bfbbbcb595c5c76106836ff2e52a66c1603
raphafoot42-debug <raphafoot42@gmail.com>
2026-03-19 19:28:04 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -29,10 +29,8 @@ export default async function handler(req, res) {
 
     const data = await response.json();
     if (data.error) throw new Error(data.error.message);
-
     let code = data.content[0].text.replace(/```html/gi, '').replace(/```/g, '').trim();
     res.status(200).json({ code });
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
